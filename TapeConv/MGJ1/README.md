19fc1332e9a45b8c2f06311445f46b5c83b42331
Aidan-Z <98519894+Aidan-Z@users.noreply.github.com>
2022-11-04 17:59:29 +0000

    Add files via upload

---
+++ TapeConv/MGJ1/README.md
@@ -0,0 +1,9 @@
+# Tape_Conversion
+Basic conversion for MGJ1 tape reel.
+
+Allows you to input the rows left on the reel (manually count them) and the code will tell you how many slots
+are left on the reel. This specific file is for the MGJ1 reel therefore it also provides the user with the amount
+of slots required to complete a new 7" reel.
+
+Can change the variables in the first few rows of code for different reel sizes,
+equation & conversion should work either way.
---
+++ TapeConv/NXJ2/NXJ2.py
@@ -0,0 +1,29 @@
+import math
+
+pi = math.pi
+depth_slot = 0.6                                           
+length_slot = 2.0                                        
+radius_start = 3.75                                         
+
+rows_remaining = input("How many rows of tape remain?: ")
+
+
+def find_slots_remaining():                                
+    row_float = float(rows_remaining)                       
+    row_int = int(row_float)                                
+    for row_float in range(1, row_int):                    
+        conv_list = list(range(1, row_int+7))              
+        it_list = [i * depth_slot for i in conv_list]       
+        radius = [i + radius_start for i in it_list]        
+        circumfrence = [(2*pi) * i for i in radius]        
+        quotient = [i / length_slot for i in circumfrence] 
+        remaining_slots = sum(quotient)                    
+        return(remaining_slots)                             
+        
+x = find_slots_remaining()                                  
+slots_remaining = round(x)                                  
+print('There are:',slots_remaining, 'slots remaining') 
+
+in_metres = (x * length_slot) / 100
+print('There are: ', in_metres, 'metres remaining')
+
